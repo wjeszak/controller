f40e8adeea41af78f8cb32552ddd99d905ff33b6
wjeszak <wjeszak@gmail.com>
2017-07-08 08:36:14 +0200

    Jeszcze common.h.

---
+++ common.h
@@ -0,0 +1,16 @@
+/*
+ * common.h
+ *
+ *  Created on: 7 lip 2017
+ *      Author: tomek
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define hi(x)			(x) >> 8
+#define lo(x) 			(x) & 0xFF
+
+
+
+#endif /* COMMON_H_ */
