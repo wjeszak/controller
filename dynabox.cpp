fc840b06590a9076a17272ab62709e2310415d64
wjeszak <wjeszak@gmail.com>
2017-08-10 14:35:02 +0200

    Tylko zmniejszenie czestotliwosci odpytywania.

--- dynabox.cpp
+++ dynabox.cpp
@@ -53,6 +53,6 @@ void Dynabox::SaveParameters()
 uint8_t Dynabox::StartupTest()
 {
 //	motor.EV_Homing();
-	timer.Assign(TIMER_MODBUS_RTU_POLL, 50, ModbusPoll);
+	timer.Assign(TIMER_MODBUS_RTU_POLL, 500, ModbusPoll);
 	return 2;
 }
